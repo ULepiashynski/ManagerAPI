68aafbe93595d2fed89d67a9056fa0f770ae183a
ULepiashynski <uladzimir.lepiashynski@soft-fx.lv>
2016-12-28 18:58:28 +0300

    Update Program.cs

--- ManagerAPI/Program.cs
+++ ManagerAPI/Program.cs
@@ -8,7 +8,7 @@ namespace ManagerAPI
 
         static void Main(string[] args)
         {
-            TtManager manager = new TtManager("tp.dev.soft-fx.eu", 8, "123qwe!");
+            TtManager manager = new TtManager("tp.dev.soft-fx.eu", 8, "*******");
             manager.Connect();
             //manager.CreateAccount();
             manager.GetAccount(100042);
